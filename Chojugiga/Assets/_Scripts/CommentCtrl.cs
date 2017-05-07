cf99d7180e5691c0765796c1a5b8a910b6b963df
toshiro <tsubasa.oshiro@gmail.com>
2017-05-07 23:40:38 +0900

    CommentCtrlをコミット。

---
+++ Chojugiga/Assets/_Scripts/CommentCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommentCtrl : MonoBehaviour {
+	public Text _result;
+	public Text _comment;
+	public Image _image;
+
+	GameManager _gameManager;
+
+	void Start() {
+		_gameManager = GameManager.GetInstance ();
+	}
+
+	public void setComment(string pStr) {
+		_comment.text = pStr;
+	}
+
+	public void setImage(Sprite pSprite) {
+		_image.sprite = pSprite;
+	}
+
+	public void setResult(string pStr) {
+		_result.text = pStr;
+	}
+}
