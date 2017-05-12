44b6284b6fb43a437dd152b8a7952a879ee30d21
toshiro <tsubasa.oshiro@gmail.com>
2017-05-13 03:24:51 +0900

    スクショ撮ってSNS投稿のサンプル実装。

---
+++ Chojugiga/Assets/_Scripts/TestScripts/ScreenShotCapture.cs
@@ -0,0 +1,59 @@
+﻿//  ScreenshotCaptor.cs
+//  http://kan-kikuchi.hatenablog.com/entry/ScreenshotCaptor
+//
+//  Created by kan.kikuchi on 2016.08.30.
+
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+
+/// <summary>
+/// スクリーンショットを撮るクラス
+/// </summary>
+public static class ScreenshotCaptor {
+
+	/// <summary>
+	/// スクリーンショットを撮る
+	/// </summary>
+	public static IEnumerator Capture(string imageName = "image.png", Action callback = null){
+
+		//スクショを保存するパスを作成
+		string imagePath = imageName;
+
+		//iOS、Android実機の時はパスにApplication.persistentDataPathを追加
+		#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
+		imagePath = Path.Combine(Application.persistentDataPath, imageName);
+		#endif
+
+		//前に撮ったスクショを削除
+		File.Delete (imagePath);
+
+		//スクリーンショットを撮る
+		Application.CaptureScreenshot(imageName);
+
+		//スクリーンショットが保存されるまで待機(最大2秒)
+		float latency = 0, latencyLimit = 2;
+		while (latency < latencyLimit) {
+			//ファイルが存在していればループ終了
+			if(File.Exists(imagePath)){
+				break;
+			}
+			latency += Time.deltaTime;
+			yield return null;
+		}
+
+		//待機時間が上限に達していたら警告表示(おそらくスクショが保存出来ていない時)
+		if(latency >= latencyLimit){
+			Debug.LogWarning("待機時間が上限に達しました！正常にスクリーンショットが保存できていません！");
+		}
+
+		//コールバックが登録されていれば実行
+		if(callback != null){
+			callback();
+		}
+
+	}
+
+
+}
---
+++ Chojugiga/Assets/_Scripts/TestScripts/ScreenShotTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScreenShotTest : MonoBehaviour {
+	public TextMeshProUGUI _label;
+	public GameObject _wall;
+
+	int counter = 0;
+	string fileName = "sc.png";
+
+	public void capture() {
+		counter++;
+		string str = "Capture Count\n" + counter;
+		_label.text = str;
+
+		// Capture
+		StartCoroutine (ScreenshotCaptor.Capture (
+			fileName,
+			callbackCapture)
+		);
+
+		_wall.SetActive (true);
+	}
+		
+	void callbackCapture() {
+		// capture success!
+		_wall.SetActive(false);
+		actionButtonShare ();
+	}
+
+	void actionButtonCapture() {
+		capture ();
+	}
+
+	void actionButtonShare() {
+		SocialConnector.SocialConnector.Share ("シェアテスト",
+			"http://www.google.co.jp", 
+			Application.persistentDataPath + "/" + fileName);
+	}
+
+}
