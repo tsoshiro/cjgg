84e46f68c1b2100a69d9b6c0bd4115e35cff3071
toshiro <tsubasa.oshiro@gmail.com>
2017-05-26 07:30:32 +0900

    Splash実装

---
+++ Chojugiga/Assets/_Scripts/FadeCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeCtrl : MonoBehaviour {
+	Image _image;
+
+	void Start() {
+		_image = this.GetComponent<Image> ();
+	}
+
+	public void Fade(float pFrom, float pTo, float pTime) {
+		iTween.ValueTo(this.gameObject, iTween.Hash("from", pFrom, "to", pTo, "time", pTime, "onupdate", "UpdateAlpha"));
+	}
+
+	void UpdateAlpha(float pValue) {
+		Color c = _image.color;
+		c.a = pValue;
+		_image.color = c;
+	}
+}
---
+++ Chojugiga/Assets/_Scripts/SplashCtrl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SplashCtrl : MonoBehaviour {
+	/// 使い方
+	/// 1. SplashCtrlをGameManagerオブジェクトにアタッチ
+	/// 2. ロゴ、フレームをそれぞれアタッチ
+
+	public FadeCtrl _logo;
+	public FadeCtrl _frame;
+	bool isSplashStarted = false;
+
+	// UIの場合
+	bool isUI;
+
+	// Use this for initialization
+	void Start () {
+		Init ();
+	}
+
+	void Init() {
+		Color c = _logo.gameObject.GetComponent<Image> ().color;
+		c.a = 0;
+		_logo.gameObject.GetComponent<Image> ().color = c;
+	}
+
+	void Update () {
+		if (!Application.isShowingSplashScreen) {
+			if (!isSplashStarted) {
+				StartCoroutine (splashFlow ());
+				isSplashStarted = true;
+			}
+		}
+
+	}
+
+	IEnumerator splashFlow () {
+		yield return 1;
+
+		// フェードイン
+		FadeIn(_logo, 0.8f);
+//		iTween.FadeTo (_logo, iTween.Hash ("a", 1.0f, "time", 0.5f));
+
+		yield return new WaitForSeconds (0.8f);
+
+		// 待機
+		yield return new WaitForSeconds(1f);
+
+		// フェードアウト
+		FadeOut(_logo, 0.5f);
+//		iTween.FadeTo (_logo, iTween.Hash ("a", 0f, "time", 0.5f));
+		yield return new WaitForSeconds (0.5f);
+
+		FadeOut (_frame, 0.5f);
+//		iTween.FadeTo (_frame, iTween.Hash ("a", 0f, "time", 0.5f));
+		yield return new WaitForSeconds (0.5f);
+
+		finishSplashFlow ();
+	}
+		
+	void FadeIn(FadeCtrl pFade, float pTime) {
+		pFade.Fade (0, 1.0f, pTime);
+	}
+
+	void FadeOut(FadeCtrl pFade, float pTime) {
+		pFade.Fade (1.0f, 0f, pTime);
+	}
+				
+	void finishSplashFlow () {
+		// 終了
+		// BGM再生
+		GameManager.GetInstance ()._audioManager.playBGM ();
+
+		// Splashシーンを非アクティブ化
+		this.gameObject.SetActive (false);
+	}
+}
